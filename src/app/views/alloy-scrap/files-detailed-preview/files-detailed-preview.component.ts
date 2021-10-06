0e8f257407ecc6479ff26a1aab0181b0ed848061
ranjitkumarray <ranjitkumar.ray@dhiomics.com>
2021-10-06 15:45:52 +0530

    api integration for detail

--- src/app/views/alloy-scrap/files-detailed-preview/files-detailed-preview.component.ts
+++ src/app/views/alloy-scrap/files-detailed-preview/files-detailed-preview.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CitGlobalConstantService } from 'src/app/services/api-collection';
 import { ApiService } from 'src/app/services/api.service';
+import { Location } from "@angular/common";
 export interface wireData {
   'VKORG': any
   'COND_TYPE': any
@@ -27,7 +28,15 @@ export interface billetData {
   'Materialnr': any
   'dRUCKSPERRE': any
 }
-
+export interface scrapData {
+  'VKORG': any
+  'COND_TYPE': any
+  'DST_CH': any
+  'DIV': any
+  'Month_year': any
+  'Model': any
+  'Amount': any
+}
 @Component({
   selector: 'app-files-detailed-preview',
   templateUrl: './files-detailed-preview.component.html',
@@ -49,6 +58,7 @@ export class FilesDetailedPreviewComponent implements OnInit {
     private router: Router,
     private apiString: CitGlobalConstantService,
     private apiMethod: ApiService,
+    private location:Location
   ) { }
 
   ngOnInit(): void {
@@ -75,17 +85,57 @@ export class FilesDetailedPreviewComponent implements OnInit {
       let tablename = Object.keys(result)
       console.log(tablename)
       if(tablename[0]==='table_wire'){
-        this.alloy_surcharge_wire = new MatTableDataSource<wireData>(JSON.parse(resultData.table_wire))
+        this.alloy_surcharge_wire = new MatTableDataSource<wireData>((resultData.table_wire))
         setTimeout(() => {
           this.alloy_surcharge_wire.paginator = this.paginator;
           this.alloy_surcharge_wire.sort = this.sort;
         });
-
+        
       }
-      this.apiMethod.popupMessage('success')
+      if(tablename[0]==='table_billet'){
+        this.alloy_surcharge_billet = new MatTableDataSource<billetData>((resultData.table_billet))
+        setTimeout(() => {
+          this.alloy_surcharge_billet.paginator = this.paginator;
+          this.alloy_surcharge_billet.sort = this.sort;
+        });
+      }
+      if(tablename[0]==='table_scrap'){
+        this.scrap_surcharge_billet = new MatTableDataSource<scrapData>((resultData.table_scrap))
+        setTimeout(() => {
+          this.scrap_surcharge_billet.paginator = this.paginator;
+          this.scrap_surcharge_billet.sort = this.sort;
+        });
+      }
     }, error => {
       this.loadingRouteConfig = false
       this.apiMethod.popupMessage('error')
     })
+  }
+  applyFilter(event: Event, searchFrom: any) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue, this.alloy_surcharge_wire,)
+    if (searchFrom === "alloy_surcharge_billet") {
+      this.alloy_surcharge_billet.filter = filterValue.trim().toLowerCase();
+      if (this.alloy_surcharge_billet.paginator) {
+        this.alloy_surcharge_billet.paginator.firstPage();
+      }
+    }
+    if (searchFrom === "alloy_surcharge_wire") {
+      this.alloy_surcharge_wire.filter = filterValue.trim().toLowerCase();
+
+      if (this.alloy_surcharge_wire.paginator) {
+        this.alloy_surcharge_wire.paginator.firstPage();
+      }
+    }
+    if (searchFrom === "scrap_surcharge_billet") {
+      this.scrap_surcharge_billet.filter = filterValue.trim().toLowerCase();
+
+      if (this.scrap_surcharge_billet.paginator) {
+        this.scrap_surcharge_billet.paginator.firstPage();
+      }
+    }
+  }
+  back(){
+    this.location.back()
   }
 }
