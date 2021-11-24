5cbb53d5cf82b63e8180350da3dac2d4e0c79ba6
ranjitkumarray <ranjitkumar.ray@dhiomics.com>
2021-11-24 13:53:34 +0530

    UI changes for allign the filters
    UI changes for allign the filters and Removed Pending With removed

--- src/app/views/my-management/my-task/offer-status/offer-status.component.ts
+++ src/app/views/my-management/my-task/offer-status/offer-status.component.ts
@@ -45,7 +45,7 @@ export class OfferStatusComponent implements OnInit {
     this.filterForm = this.fb.group({
       search_string: [''],
       customer: [''],
-      pending_with: [''],
+      // pending_with: [''],
       status: [''],
       created: [''],
       offerid: [''],
