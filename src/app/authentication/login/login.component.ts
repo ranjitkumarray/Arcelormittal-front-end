d3be8f68703c157639a67f9ab13ba76f8558677b
Vinayak-200 <vinayak.bhandari@dhiomics.com>
2021-12-08 17:08:17 +0530

    latest

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -10,55 +10,61 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-login:any=FormGroup;
-hide=true;
-ot :any;
-loginData :any;
+  login: any = FormGroup;
+  hide = true;
+  loginData: any;
 
-  constructor(private fb:FormBuilder,
-              private apimethod : ApiService ) { }
+  constructor(private fb: FormBuilder,
+    private apimethod: ApiService) { }
 
-  
-  
+
+
   ngOnInit(): void {
-    this.login=this.fb.group({
-     'email':['',[
-      Validators.required,
-      Validators.email
-     ]],
-     'password':['',[
-      Validators.required,
-      Validators.pattern
-     ]]
+    this.login = this.fb.group({
+      'email': ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      'password': ['', [
+        Validators.required,
+       
+      ]]
     })
   }
-  obj :any =[{"email":'sayeesh'}]
- 
-loginSubmit( form:FormControl){
+  
 
-      console.log(this.login) 
-      this.loginData = this.login
+  loginSubmit(form: FormControl) {
 
-      if(this.login.valid==true){
-        this.apimethod.popupMessage('success','good')
-      }
-      else {
-        this.apimethod.popupMessage('error','error')
-      }
+    console.log(this.login)
+    // this.loginData = this.login
 
-//   if(this.loginData.email != false && this.loginData.password != false)  {
-//     this.apimethod.popupMessage('success', 'success')
-//   }
-//   else if(this.loginData.email == false && this.loginData.password != false){
-//     this.apimethod.popupMessage('error', 'Email required')
-//   }
-//   else if(this.loginData.email != false && this.loginData.password == false){
-//     this.apimethod.popupMessage('error', 'Password required')
-//   }
-//   else
-//    {
-//     this.apimethod.popupMessage('error','Email and Password required')
-//   }
-}
+    // if (this.login.valid == true) {
+    //   this.apimethod.popupMessage('success', 'good')
+    // }
+    // else {
+    //   this.apimethod.popupMessage('error', 'error')
+    if(this.login.status =='VALID'){
+      this.apimethod.popupMessage('success', 'good')
+    }
+    else{
+      this.apimethod.popupMessage('error', 'error')
+    }
+    
+    }
+
+    //   if(this.loginData.email != false && this.loginData.password != false)  {
+    //     this.apimethod.popupMessage('success', 'success')
+    //   }
+    //   else if(this.loginData.email == false && this.loginData.password != false){
+    //     this.apimethod.popupMessage('error', 'Email required')
+    //   }
+    //   else if(this.loginData.email != false && this.loginData.password == false){
+    //     this.apimethod.popupMessage('error', 'Password required')
+    //   }
+    //   else
+    //    {
+    //     this.apimethod.popupMessage('error','Email and Password required')
+    //   }
+  // }
 
 }
