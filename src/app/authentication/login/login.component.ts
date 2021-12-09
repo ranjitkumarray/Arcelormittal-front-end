fdaa0892c62d4b550030f92364d256bdd552f048
Vinayak-200 <vinayak.bhandari@dhiomics.com>
2021-12-09 18:16:14 +0530

    latest

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Type } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { range } from 'lodash';
 
@@ -7,13 +7,13 @@ import { ApiService } from 'src/app/services/api.service';
 import user from './user.json'
 
 interface USERS {
-  name:string,
-  email:string,
-  password:string,
-  phonenumber:number,
-  address:string,
-  usertype:string,
-  accessstoken:string
+  name: string,
+  email: string,
+  password: string,
+  phonenumber: number,
+  address: string,
+  usertype: string,
+  accessstoken: string
 }
 
 @Component({
@@ -25,17 +25,13 @@ export class LoginComponent implements OnInit {
   login: any = FormGroup;
   hide = true;
   loginData: any;
-
-  Users=user
-
+  Users = user;
+  a: any;
   constructor(private fb: FormBuilder,
     private apimethod: ApiService,
-    private router:Router) {
-
-      console.log(this.Users)
-     
-    }
-
+    private router: Router) {
+    console.log(this.Users)
+  }
 
 
   ngOnInit(): void {
@@ -46,33 +42,55 @@ export class LoginComponent implements OnInit {
       ]],
       'password': ['', [
         Validators.required,
-       
+
       ]]
     })
   }
-  
-  a:any
+
+
   loginSubmit(form: FormGroup) {
-    // console.log(this.login)
-    for(let i in this.Users){
-      if(this.login.value.email==this.Users[i].email && this.login.value.password==this.Users[i].password){
+    console.log(this.login)
+    if (this.login.status == "VALID") {
+      
+
+      for (let i in this.Users) {
+        if (this.login.value.email == this.Users[i].email && this.login.value.password == this.Users[i].password) {
+
+          this.a = true
+          // this.router.navigate(['/alloy-scrap/history/fileDetails/'])
+        }
+        else{
+          this.apimethod.popupMessage('error','wrong,Enter again!!')
+        }
+      }
+        }
+        else {
+          this.apimethod.popupMessage('error','Invalid Details')
+        }  
         
-        this.a=true
-        // this.router.navigate(['/alloy-scrap/history/fileDetails/'])
+        console.log(this.login.status)
+      
+      
 
-       
-       }
-       else  {
-        this.apimethod.popupMessage('error','Bad')
-    }
-    if(this.a==true){
-      this.apimethod.popupMessage('success','Good')
-      this.router.navigate(['/alloy-scrap/history/fileDetails/'])
-    }
+
+      // for (let i in this.Users) {
+      //   if (this.login.value.email == this.Users[i].email && this.login.value.password == this.Users[i].password) {
+
+      //     this.a = true
+      //     // this.router.navigate(['/alloy-scrap/history/fileDetails/'])
+
+      //   }
+      //   else {
+      //     this.apimethod.popupMessage('error', 'Somthing went worng!!')
+      //   }
+      if (this.a == true) {
+        this.apimethod.popupMessage('success', 'Login Successfull!!')
+        this.router.navigate(['/alloy-scrap/upload/'])
+      }
+
+
 
     
-    
-    }    
 
     // console.log(this.Users[0].email)
     // console.log(this.login)
@@ -92,22 +110,22 @@ export class LoginComponent implements OnInit {
     // else{
     //   this.apimethod.popupMessage('error', 'Login Failed')
     // }
-    
-    }
 
-    //   if(this.loginData.email != false && this.loginData.password != false)  {
-    //     this.apimethod.popupMessage('success', 'success')
-    //   }
-    //   else if(this.loginData.email == false && this.loginData.password != false){
-    //     this.apimethod.popupMessage('error', 'Email required')
-    //   }
-    //   else if(this.loginData.email != false && this.loginData.password == false){
-    //     this.apimethod.popupMessage('error', 'Password required')
-    //   }
-    //   else
-    //    {
-    //     this.apimethod.popupMessage('error','Email and Password required')
-    //   }
+  }
+
+  //   if(this.loginData.email != false && this.loginData.password != false)  {
+  //     this.apimethod.popupMessage('success', 'success')
+  //   }
+  //   else if(this.loginData.email == false && this.loginData.password != false){
+  //     this.apimethod.popupMessage('error', 'Email required')
+  //   }
+  //   else if(this.loginData.email != false && this.loginData.password == false){
+  //     this.apimethod.popupMessage('error', 'Password required')
+  //   }
+  //   else
+  //    {
+  //     this.apimethod.popupMessage('error','Email and Password required')
+  //   }
   // }
 
 }
