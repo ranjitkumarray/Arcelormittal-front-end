b1be9ec0fd7704b550c9f90f4e83a5220b0fe81f
ranjitkumarray <ranjitkumar.ray@dhiomics.com>
2021-12-09 17:52:43 +0530

    design change on add and edit popup

--- src/app/views/smb/extra-certificate-list/extra-certificate-list.component.ts
+++ src/app/views/smb/extra-certificate-list/extra-certificate-list.component.ts
@@ -67,7 +67,9 @@ export class ExtraCertificateListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getCertificate()
+    // this.getCertificate()
+    this.dataSource = new MatTableDataSource<certificateData>(this.data)
+
   }
   //getting uploaded history of alloy scrap 
   getCertificate() {
@@ -115,6 +117,7 @@ export class ExtraCertificateListComponent implements OnInit {
           panelClass: 'my-full-screen-dialog',
           autoFocus: false,
           maxHeight: '90vh',
+          maxWidth: '40vw',
           data: {
             content: '',
             addURL: this.apiStringURL.add,
