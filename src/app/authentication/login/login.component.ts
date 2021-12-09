d9f6079f0624e5820696c01d08619c859be135cd
Sayeesh123 <92074452+Sayeesh123@users.noreply.github.com>
2021-12-09 13:26:49 +0530

    latest

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,7 +1,20 @@
 import { Component, OnInit, Type } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { range } from 'lodash';
 
 import { ApiService } from 'src/app/services/api.service';
+import user from './user.json'
+
+interface USERS {
+  name:string,
+  email:string,
+  password:string,
+  phonenumber:number,
+  address:string,
+  usertype:string,
+  accessstoken:string
+}
 
 @Component({
   selector: 'app-login',
@@ -13,8 +26,15 @@ export class LoginComponent implements OnInit {
   hide = true;
   loginData: any;
 
+  Users=user
+
   constructor(private fb: FormBuilder,
-    private apimethod: ApiService) { }
+    private apimethod: ApiService,
+    private router:Router) {
+
+      console.log(this.Users)
+     
+    }
 
 
 
@@ -32,9 +52,28 @@ export class LoginComponent implements OnInit {
   }
   
 
-  loginSubmit(form: FormControl) {
+  loginSubmit(form: FormGroup) {
+    // console.log(this.login)
+    for(let i in this.Users){
+      if(this.login.value.email==this.Users[i].email && this.login.value.password==this.Users[i].password){
+        this.apimethod.popupMessage('success','Good')
+        // this.router.navigate(['/alloy-scrap/history/fileDetails/'])
 
-    console.log(this.login)
+       
+       }
+       else if(this.login.value.email!=this.Users[i].email && this.login.value.password!=this.Users[i].password) {
+        this.apimethod.popupMessage('error','Bad')
+        console.log(this.Users[i].email)
+    }
+
+    
+    
+    }    
+
+    // console.log(this.Users[0].email)
+    // console.log(this.login)
+
+
     // this.loginData = this.login
 
     // if (this.login.valid == true) {
@@ -42,12 +81,13 @@ export class LoginComponent implements OnInit {
     // }
     // else {
     //   this.apimethod.popupMessage('error', 'error')
-    if(this.login.status =='VALID'){
-      this.apimethod.popupMessage('success', 'good')
-    }
-    else{
-      this.apimethod.popupMessage('error', 'error')
-    }
+
+    // if(this.login.status == 'VALID'){
+    //   this.apimethod.popupMessage('success', 'Login Succes')
+    // }
+    // else{
+    //   this.apimethod.popupMessage('error', 'Login Failed')
+    // }
     
     }
 
