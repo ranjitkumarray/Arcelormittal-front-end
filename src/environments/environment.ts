83917a4da4d042d3f8468dc5e45bd9a28eabf079
ranjitkumarray <ranjitkumar.ray@dhiomics.com>
2021-12-29 18:29:28 +0530

    hosting with https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   // local host domain 
-  domain:"http://127.0.0.1:5000/",
+  // domain:"http://127.0.0.1:5000/"
   // dev domain 
-  // domain: "https://18.159.253.5:5000/",
-  newDomain: 'https://1jzvdcwt24.execute-api.eu-central-1.amazonaws.com/development/'
+  domain: "https://smbpriceapi.dcc-am.com/"
+  //prod domain
 };
 
 /*
