471ff54e36a44e333b5192dcf36115c19decd16c
Sayeesh123 <92074452+Sayeesh123@users.noreply.github.com>
2022-01-04 17:19:23 +0530

    latest

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -2,24 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
 import { FormGroup } from '@angular/forms';
+import { CitGlobalConstantService } from 'src/app/services/api-collection';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  loadingRouteConfig : boolean = false
   ForgotPassword: any = FormGroup;
-  emailPattern = "^[a-z0-9._%+-]+@['gmail']+\.[com]{2,4}$";
-  constructor(private fb:FormBuilder,private apimethod:ApiService) { }
+  email_Pattern = "^[a-z0-9._%+-]+@['gmail']+\.[com]{2,4}$";
+  constructor(private _formbuilder : FormBuilder,
+              private apimethod : ApiService,
+              private apistring : CitGlobalConstantService,
+              private router : Router) { }
 
   ngOnInit(): void {
-    this.ForgotPassword=this.fb.group({
+    this.ForgotPassword=this._formbuilder.group({
       'email':['',[Validators.required,
         Validators.email,
-        Validators.pattern(this.emailPattern)
+        Validators.pattern(this.email_Pattern)
       ]]
     })
-  }
   
+  }
+  data :any
+  reset_password(){
+    // console.log(this.ForgotPassword.value.email)   
+    this.apimethod.get_request_Param(this.apistring.userAccess.forgot_password, { email : this.ForgotPassword.value.email}).subscribe((result: any) => {
+      console.log(result.status)
+      if(result.status==='Valid Email'){
+        this.apimethod.popupMessage('success',result.status)
+        this.router.navigate(['/auth/reset-password'])
+      }
+      else {
+        this.apimethod.popupMessage('error',result.status)
+      }
+  })
+}
 
 }
--- src/app/authentication/reset-password/reset-password.component.ts
+++ src/app/authentication/reset-password/reset-password.component.ts
@@ -1,5 +1,9 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { timeStamp } from 'console';
+import { CitGlobalConstantService } from 'src/app/services/api-collection';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,18 +11,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
-resetPassword:any=FormGroup;
-  constructor(private fb: FormBuilder) { }
+  resetPassword : any = FormGroup;
+  constructor(private _formbuilder: FormBuilder,
+              private apiMethod : ApiService,
+              private apiString : CitGlobalConstantService) { }
 
   ngOnInit(): void {
-    this.resetPassword = this.fb.group({
-      'NewPassword': ['', [
-        Validators.required,
-      ]],
-      'ConfirmPassword': ['', [
-        Validators.required,
-
-      ]]
+    this.resetPassword = this._formbuilder.group({
+      'NewPassword': ['',[Validators.required,Validators.pattern('[0-9]{8}')]],
+      'ConfirmPassword': ['',[Validators.required,Validators.pattern('[0-9]{8}')]]
+    })
+  }
+  reset(){
+    console.log(this.resetPassword.value)
+    this.apiMethod.get_request_Param(this.apiString.userAccess.reset_password,{NewPassword : this.resetPassword.value.NewPassword, ConfirmPassword : this.resetPassword.value.ConfirmPassword}).subscribe((result:any)=>{
+      console.log(result)
+      if(result.status==200){
+        this.apiMethod.popupMessage('success',result.status)
+      }
+      else {
+        this.apiMethod.popupMessage('error',result.status)
+      }
     })
   }
 
